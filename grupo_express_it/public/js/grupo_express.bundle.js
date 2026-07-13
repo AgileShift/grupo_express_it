414a25314219ba5072beda2130a510acf430485d
Jeans K. Real <jkrb7@hotmail.com>
2026-07-13 16:03:51 -0600

    Child Table on v16, fixes everithing that we have customized

--- grupo_express_it/public/js/grupo_express.bundle.js
+++ grupo_express_it/public/js/grupo_express.bundle.js
@@ -8,56 +8,30 @@ frappe.ui.form.ControlTable = class ControlTable extends frappe.ui.form.ControlT
 		if (!['Policy Item', 'Policy CIF Cost', 'Policy Nationalization Cost'].includes(this.df.options))
 			return; // No Personalization. Default controller
 
-		this.refresh_input = this.control_refresh_input_append_footer; // Override Control Function
-
-		// Override Grid Functions See: frappe/frappe/public/js/frappe/form/grid.js
-		this.grid.df.on_setup = this.df_on_setup; // At execution the grid fields are not yet set. Only html elements
-		this.grid.make_head = this.grid_make_head; // Override to Remove: configure_columns from header and header_search
-		this.grid.setup_visible_columns = this.grid_setup_visible_columns; // Override Visible Columns Limit of 11
-	}
-
-	control_refresh_input_append_footer() {
-		super.refresh_input(); // Call Super of the ControlTable
-		this.grid_make_footer.call(this.grid); // We Use call to set the context to the grid
-	}
-
-	df_on_setup(grid) {
-		grid.grid_pagination.page_length = 75; // Increase Pagination Limit
-		grid.form_grid.append('<div class="grid-footer-row disable-click bold"></div>'); // Make Footer Row for totals
-	}
-
-	grid_make_head() {
-		if (this.prevent_build) return;
-
-		if (this.header_row) {
-			$(this.parent).find('.grid-heading-row .grid-row').remove(); // Remove to redraw
+		// At execution the grid fields are not yet set. Only html elements
+		this.grid.df.on_setup = (grid) => {
+			grid.form_grid.append('<div class="grid-footer-row disable-click bold"></div>'); // Make Footer Row for totals
 		}
 
-		this.header_row = new GridRow({
-			parent: $(this.parent).find('.grid-heading-row'),
-			docfields: this.docfields,
-			grid: this
-		});
-	}
-
-	grid_setup_visible_columns() {
-		this.df.in_place_edit = true; // To Hide the Edit Icon FIXME: This Runs many times. More than needed!
-
-		if (this.visible_columns && this.visible_columns.length > 0) return; // Just Run Once per Grid
-
-		// TODO: Optimize Using: for of this.docfields. With or without Logical (AND OR) Assignment. Setting the Array Length
-		this.visible_columns = this.docfields.filter(df => df.in_list_view).map(df => [df, df.columns]);
-		this.total_columns = this.docfields
-			.filter((df) => frappe.model.is_numeric_field(df.fieldtype) && !['exchange_rate', 'fob_unit_price', 'unit_price'].includes(df.fieldname))
-			.map(df => df.fieldname);
+		// Override Grid Functions See: frappe/frappe/public/js/frappe/form/grid.js
+		this.refresh_input = () => {
+			super.refresh_input();                 // Call Super of the ControlTable
+			this.grid_make_footer.call(this.grid); // We Use call to set the context to the grid
+		};
 	}
 
 	grid_make_footer() {
 		if (this.prevent_build) return; // See grid.make_head(). if (this.data.length === 0) return -> Avoided
 
 		if (this.footer_row) {
-			this.form_grid.find(".grid-footer-row .grid-row").remove(); // Remove to redraw. Created on df_on_setup()
+			this.form_grid.find(".grid-footer-row .grid-row").remove(); // Remove to redraw. Created on df.on_setup()
 		}
+
+		if (!this.data.length > 0) return;
+
+		this.total_columns = this.docfields
+			.filter((df) => frappe.model.is_numeric_field(df.fieldtype) && !['exchange_rate', 'fob_unit_price', 'unit_price'].includes(df.fieldname))
+			.map(df => df.fieldname);
 
 		this.footer_row = new GridRow({
 			parent: this.form_grid.find('.grid-footer-row'),
